bafc140862f8a52530338652cdb99dc0739ebd89
Ahmed Salim <dr.medo@me.com>
2020-01-16 05:18:29 +0300

    Adding Solar Bootstrap and Hardcoding the membershiptypes

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace VidlyAuth
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-solar.css",
                       "~/Content/site.css"));
         }
     }
--- Models/MembershipType.cs
+++ Models/MembershipType.cs
@@ -14,5 +14,12 @@ namespace VidlyAuth.Models
         public double Discount { get; set; }
         public decimal SignUpFee { get; set; }
 
+        // Adding Static Membership Types Hard Coded
+
+        public static byte Unknown { get; set; } = 0;
+        public static byte PayAsYouGo { get; set; } = 1;
+        public static byte Monthly { get; set; } = 2;
+        public static byte Quarterly { get; set; } = 3;
+        public static byte Annual { get; set; } = 4;
     }
 }
