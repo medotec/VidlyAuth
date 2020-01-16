455ab688339760e2c645157685bb5e068159fbad
Ahmed Salim <dr.medo@me.com>
2020-01-16 06:57:06 +0300

    Adding the Movies and Customers Controlles with their Index Views

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyAuth.Models;
+
+namespace VidlyAuth.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CustomersController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Customers
+        public ActionResult Index()
+        {
+            IEnumerable<Customer> customers = _dbContext.Customers.Include(c => c.MembershipType).ToList();
+            return View();
+        }
+    }
+}
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyAuth.Models;
+
+namespace VidlyAuth.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public MoviesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Movie
+        public ActionResult Index()
+        {
+            IEnumerable<Movie> movies = _dbContext.Movies.Include(m => m.Genre).ToList();
+            return View();
+        }
+    }
+}
