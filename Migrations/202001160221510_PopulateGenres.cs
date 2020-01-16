b6a860a3f0e47a6fecb159b1a21078d2a4eb7345
Ahmed Salim <dr.medo@me.com>
2020-01-16 05:26:18 +0300

    Populating Genres Table and Adding Hardcoded vales to the Class

---
+++ Migrations/202001160221510_PopulateGenres.cs
@@ -0,0 +1,27 @@
+﻿namespace VidlyAuth.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateGenres : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"INSERT INTO dbo.Genres (Id, Name)
+                VALUES
+                (1, 'Adventure'),
+                (2, 'Action'),
+                (3, 'Drama'),
+                (4, 'Comedy'),
+                (5, 'Thriller'),
+                (6, 'Horror'),
+                (7, 'Musical'),
+                (8, 'Documentary');");
+        }
+        
+        public override void Down()
+        {
+            Sql("DELETE FROM dbo.Genres WHERE Id BETWEEN 1 AND 8;");
+        }
+    }
+}
--- Models/Genre.cs
+++ Models/Genre.cs
@@ -13,5 +13,17 @@ namespace VidlyAuth.Models
         [Required]
         [StringLength(20)]
         public string Name { get; set; }
+
+        // Genres Hard Coded
+
+        public static byte Adventure { get; set; }= 1;
+        public static byte Action { get; set; }= 2;
+        public static byte Drama { get; set; }= 3;
+        public static byte Comedy { get; set; }= 4;
+        public static byte Thriller { get; set; }= 5;
+        public static byte Horror { get; set; }= 6;
+        public static byte Musical { get; set; }= 7;
+        public static byte Documentary { get; set; }= 8;
+
     }
 }
